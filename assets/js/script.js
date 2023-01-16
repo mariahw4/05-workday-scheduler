d9636f73ea823704e721b7c9a93bdb055f400edd
Mariah Wear <wearmariahk@gmail.com>
2023-01-15 17:36:05 -0700

    final commenting of JS, updating README with deployed link and gif

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,5 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-// $(function () {
-$(document).ready(function () {
 
-  var dayDisplayEl = $('#currentDay');
-  function displayDay() {
-    var rightNow = dayjs().format('dddd, MMM DD, YYYY');
-    dayDisplayEl.text(rightNow);
-  }
-  displayDay();
+$(document).ready(function () {
 
     // saveBtn click listener 
     $(".saveBtn").on("click", function () {
@@ -28,16 +18,16 @@ $(document).ready(function () {
 
     // loop over time blocks
     $(".time-block").each(function () {
-        var blockTime = parseInt($(this).attr("id").split("hour-")[1]);
-        console.log(blockTime);
+        var unitTime = parseInt($(this).attr("id").split("hour-")[1]);
+        console.log(unitTime);
 
         // To check the time and add the classes for background indicators
-        if (blockTime < timeNow) {
+        if (unitTime < timeNow) {
             $(this).removeClass("future");
             $(this).removeClass("present");
             $(this).addClass("past");
         }
-        else if (blockTime == timeNow) {
+        else if (unitTime == timeNow) {
             $(this).addClass("present");
             $(this).removeClass("past");
             $(this).removeClass("future");
@@ -66,6 +56,13 @@ $(document).ready(function () {
   
     hourTracker();
 
+    // Display the day and date
+    var dayDisplayEl = $('#currentDay');
+  function displayDay() {
+    var rightNow = dayjs().format('dddd, MMM DD, YYYY');
+    dayDisplayEl.text(rightNow);
+  }
+  displayDay();
   })
 
   
